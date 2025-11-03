f3dff036be84abec1bb716ff9239f8552ee78071
leslie291 <lesles078@gmail.com>
2025-11-02 22:55:49 -0800

    Refactor command strings to use double quotes

--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
-import 'dotenv';
-import { getRPSChoices } from './game.js';
-import { capitalize, InstallGlobalCommands } from './utils.js';
+import "dotenv";
+import { getRPSChoices } from "./game.js";
+import { capitalize, InstallGlobalCommands } from "./utils.js";
 
 // Get the game choices from game.js
 function createCommandChoices() {
@@ -19,8 +19,8 @@ function createCommandChoices() {
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
+  name: "test",
+  description: "Basic command",
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 1, 2],
@@ -28,20 +28,20 @@ const TEST_COMMAND = {
 
 // Command containing options
 const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
+  name: "challenge",
+  description: "Challenge to a match of rock paper scissors",
   options: [
     {
       type: 3,
-      name: 'object',
-      description: 'Pick your object',
+      name: "object",
+      description: "Pick your object",
       required: true,
       choices: createCommandChoices(),
     },
     {
       type: 10,
-      name: 'wager',
-      description: 'How much do you want to gamble?',
+      name: "wager",
+      description: "How much do you want to gamble?",
       required: false,
     },
   ],
@@ -52,52 +52,67 @@ const CHALLENGE_COMMAND = {
 
 // Command to display leaderboard
 const LEADERBOARD_COMMAND = {
-  name: 'leaderboard',
-  description: 'Displays leaderboard',
+  name: "leaderboard",
+  description: "Displays leaderboard",
   type: 1,
   integration_types: [0, 1],
-  contexts: [0, 1, 2]
+  contexts: [0, 1, 2],
 };
 
 // Command to update the leaderboard
 const UPDATE_LEADERBOARD_COMMAND = {
-  name: 'update_leaderboard',
-  description: 'Used by games to update points and games played on the leaderboard',
+  name: "update_leaderboard",
+  description:
+    "Used by games to update points and games played on the leaderboard",
   options: [
     {
       type: 3,
-      name: 'user',
-      description: 'User to update',
+      name: "user",
+      description: "User to update",
       required: true,
     },
     {
       type: 4,
-      name: 'points',
-      description: 'Points update ammount',
+      name: "points",
+      description: "Points update ammount",
       required: true,
     },
     {
       type: 4,
-      name: 'games',
-      description: 'Games played update ammount',
+      name: "games",
+      description: "Games played update ammount",
       required: false,
     },
   ],
   type: 1,
   integration_types: [0, 1],
-  contexts: [0, 1, 2]
+  contexts: [0, 1, 2],
 };
 
 const WORDLE_COMMAND = {
-  name: 'dwordle',
-  description: 'Start a game of Wordle',
+  name: "dwordle",
+  description: "Start a game of Wordle",
   type: 1,
   integration_types: [0, 1],
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, LEADERBOARD_COMMAND,
-UPDATE_LEADERBOARD_COMMAND, WORDLE_COMMAND];
+// Help / About command with display-components contexts
+const HELP_COMMAND = {
+  name: "help",
+  description: "Show game rules, points, rewards, and commands",
+  type: 1,
+  integration_types: [0, 1], // guild + user install [web:15]
+  contexts: [0, 1, 2], // guild, bot DM, private channel [web:15]
+};
 
+const ALL_COMMANDS = [
+  TEST_COMMAND,
+  CHALLENGE_COMMAND,
+  LEADERBOARD_COMMAND,
+  UPDATE_LEADERBOARD_COMMAND,
+  WORDLE_COMMAND,
+  HELP_COMMAND,
+];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
